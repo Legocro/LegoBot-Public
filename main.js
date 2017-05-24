aec9506beef472da84b4a22fb7eae1ba03a525a3
Legocro <marin.chy@live.com>
2017-05-24 21:07:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -9,9 +9,3 @@ Client.bot.on("error" , (e) => {
 console.log(e.stack);
 });
 Client.bot.login(Client.settings.token);
-
-app.set('port', (process.env.PORT || 5000));
-
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
