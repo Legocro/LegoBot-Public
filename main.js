2457941f9417e36d7d945c444da53c007239ed3b
Legocro <marin.chy@live.com>
2017-05-24 19:52:25 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,4 +5,7 @@ const Client = {
 Client.bot.on("ready" , () => {
 	Client.bot.channels.get('266770415051997197').send("Wus good");
 });
+Client.bot.on("error" , (e) => {
+console.log(e.stack);
+});
 Client.bot.login(Client.settings.token);
