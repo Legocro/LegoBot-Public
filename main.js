cf4cadeb907111b3b020b6f05534395ad1491f58
Legocro <marin.chy@live.com>
2017-05-24 19:49:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ const Discord = require("discord.js");
 const Client = { 
       bot : Discord.Client(),
       settings : require("../settings.json")}
+Client.bot.on("ready" , () => {
+	Client.bot.channels.get('266770415051997197').send("Wus good");
+});
 Client.bot.login(Client.settings.token);
