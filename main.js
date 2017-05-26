704f3ceda6d303ea6f3507e3de5ee41355a0f037
Legocro <marin.chy@live.com>
2017-05-26 18:44:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -9,3 +9,9 @@ Client.bot.on("error" , (e) => {
 console.log(e.stack);
 });
 Client.bot.login(Client.settings.token);
+http = require 'http'
+handle = (req, res) -> res.end "hit"
+
+server = http.createServer handle
+
+server.listen process.env.PORT || 5000
