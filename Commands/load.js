9054ba5c47fd30574e1076e212c3b660777ed889
Legocro <marin.chy@live.com>
2017-05-27 12:41:58 +0200

    Update load.js

--- Commands/load.js
+++ Commands/load.js
@@ -1,7 +1,7 @@
 exports.cmd = (Client , m , args) => {
 	console.log(args[0], args);
 	if (!(args[0] in Client.commands)){
-		Client.commands[args[0]] = require(`./${args[0]}.js`);
+		Client.commands[args[0]] = require(`./Commands/${args[0]}.js`);
 		m.channel.send(`Successfully loaded command \`${args[0]}\``)
 	} else {
 		m.channel.send(`Command \`${args[0]}\` is already loaded, please use \`reload\` instead.`);
