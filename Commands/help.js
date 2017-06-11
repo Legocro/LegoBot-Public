7aab503f7b8af86d4c58b76d681f6ae8efea5eac
Legocro <marin.chy@live.com>
2017-06-11 10:43:54 +0200

    Add files via upload

---
+++ Commands/help.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js")
+exports.cmd = (Client , m , args) => {
+try{
+	let help = new Discord.RichEmbed();
+	help.addField("**Prefix**" , "m!");
+	help.addField("**Commands**" , Object.keys(Client.commands).join(","));
+	help.addField("**Owner**" , "<@204372456339800065>");
+	m.author.send({embed : help});
+}catch(e){
+	m.channel.send(`Error: \`\`\`${e.stack}\`\`\``);
+}
+}
+exports.config = {
+	permlevel : 0
+}
